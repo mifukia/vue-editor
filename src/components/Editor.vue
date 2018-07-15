8922f9c3d8030a22af4c005a121df39d3211016a
mifukia <mifukiahelp@gmail.com>
2018-07-15 23:23:45 +0900

    css

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,17 +1,28 @@
 <template>
   <div id="editor">
-    <h1>エディター画面</h1>
-    <ul>
-      <li>{{user.displayName}}</li>
-      <li class="myphoto"><img :src="user.photoURL" alt=""></li>
-    </ul>
-    <button @click="logout">
-      ログアウト
-    </button>
-    <div class="editorWrapper">
-      <textarea class="markdown" v-model="markdown"></textarea>
-      <div class="preview" v-html="preview()"></div>
+    <div class="userArea">
+      <ul>
+        <li class="myphoto"><img :src="user.photoURL" alt=""></li>
+        <li>{{user.displayName}}</li>
+      </ul>
+      <button @click="logout" class="logout">
+        ログアウト
+      </button>
     </div>
+    <main class="main">
+      <div class="memoListWrapper">
+        <div class ="memoList" v-for ="(memo, index) in memos" @click ="selectMemo( index)" :data-selected ="index == selectedIndex">
+          <p class="memoTitle">
+            {{displayTitle(memo.markdown)}}
+          </p>
+        </div>
+        <button class="addMemoBtn" @click="addMemo">
+          メモの追加
+        </button>
+      </div>
+      <textarea class="markdown" v-model="memos[selectedIndex].markdown"></textarea>
+      <div class="preview" v-html="preview()"></div>
+    </main>
   </div>
 </template>
 
@@ -22,40 +33,99 @@ export default {
   props:['user'],
   data () {
     return {
-      markdown:""
+      memos:[
+        {
+          markdown:""
+        }
+      ],
+      selectedIndex:0
     }
   },
   methods: {
     logout(){
       firebase.auth().signOut();
     },
+    addMemo(){
+      this.memos.push({
+        markdown:"無題のメモ"
+      })
+      console.log(this.memos)
+    },
+    selectMemo(index){
+      this.selectedIndex = index;
+    },
     preview(){
-      return marked(this.markdown);
+      return marked(this.memos[this.selectedIndex].markdown);
+    },
+    displayTitle(text){
+      return text.split(/\n/)[0];
     }
   }
 }
 </script>
 
 <style lang="scss">
+  #editor{
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+  .userArea{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .logout{
+    height: 3em;
+  }
   ul{
+    display: flex;
+    align-items: center;
+    padding: 0;
+    margin: 0;
     li{
       list-style: none;
       &.myphoto{
+        margin-right: 1em;
         img{
-          width: 100px;
+          width: 50px;
         }
       }
     }
   }
-  .editorWrapper{
-    display:flex;
+  .main{
+    display: flex;
+    justify-content: space-between;
+  }
+  .memoListWrapper{
+    width: 19%;
+    border-top: 1px solid #000;
+  }
+  .memoList{
+    padding: 10px;
+    box-sizing: border-box;
+    text-align: left;
+    border-bottom: 1px solid #000;
+    &:nth-child(even){
+      background: #ccc;
+    }
+    &[data-selected="true"]{
+      background: #ccf;
+    }
+  }
+  .memoTitle{
+    height: 1.5em;
+    margin: 0;
+  }
+  .addMemoBtn{
+    margin-top: 20px;
   }
   .markdown{
-    width:50%;
-    height:500px;
+    width: 40%;
+    height: 500px;
   }
   .preview{
-    width: 50%;
+    width: 40%;
     text-align: left;
+    border: 1px solid #ccf;
   }
 </style>
