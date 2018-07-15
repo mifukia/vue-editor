f0aa69debac473d6b122817f6c514332207f149f
mifukia <mifukiahelp@gmail.com>
2018-07-16 06:03:59 +0900

    css add

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -27,7 +27,7 @@
         </button>
       </div>
       <textarea class="markdown" v-model="memos[selectedIndex].markdown"></textarea>
-      <div class="preview" v-html="preview()"></div>
+      <div class="preview markdown-body" v-html="preview()"></div>
     </main>
   </div>
 </template>
